f9f7ba2955226fe7015a77f45e95f63eebe5c9ff
Mik1810 <michael.piccirilli@student.univaq.it>
2023-02-28 10:19:27 +0100

    Added the function to load an image

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,27 @@
 const button = document.querySelector("#test");
 button.onclick = () => document.title = "Ciao";
+
+
+/*document.getElementById("file-input").onchange = function (e) {
+    console.log("Test");
+    console.log(e.target);
+    
+}*/
+
+
+function onFileSelected(event) {
+    var selectedFile = event.target.files[0];
+    var reader = new FileReader();
+  
+    var imgtag = document.getElementById("myimage");
+    imgtag.title = selectedFile.name;
+  
+    reader.onload = function(event) {
+      imgtag.src = event.target.result;
+    };
+  
+    reader.readAsDataURL(selectedFile);
+    const inputBro = document.querySelector("#input-file");
+    inputBro.parentNode.removeChild(inputBro);
+    document.getElementById("myimage").style.display = "block";
+  }
