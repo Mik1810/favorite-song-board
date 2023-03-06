790d28165c5f79085fd1dd7ffc65fd92c40ec9c7
Mik1810 <michael.piccirilli@student.univaq.it>
2023-03-06 22:27:03 +0100

    Added prefetching of datas from localDB, corrected some issues that not display correctly the image, working on CSS

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,9 @@ const input = document.getElementById("input-file");
 const myImage = document.getElementById("myimage");
 const imagePublished = document.getElementById("imagePublished");
 
+fetchLocalDB();
+fetchPost();
+
 input.addEventListener('change', (event) => {
   const imageS = event.target.files[0];
 
@@ -11,17 +14,15 @@ input.addEventListener('change', (event) => {
   console.log(imageS);
 
   let x = reader.readAsDataURL(imageS);
-  console.log(reader.result);
+  //console.log(reader.result);
 
   reader.addEventListener('load', () => {
-    localStorage.setItem('input-file', reader.result);
-    var thumbnail = localStorage.getItem("input-file");
+    localStorage.setItem("image", reader.result);
+    var thumbnail = localStorage.getItem("image");
     myImage.setAttribute('src', thumbnail);
     myImage.style.display = "block";
     input.style.display = "none";
   });
-  
-  
 });
 
 
@@ -51,13 +52,9 @@ const inputTitle = document.querySelector("#song-name");
 const inputArtist = document.querySelector("#artist-name") 
 const inputDescription = document.querySelector("#description-area");
 
-const firstColumn = document.querySelector("#songPublished");
-const secondColumn = document.querySelector("#artistPublished");
-const thirdColumn = document.querySelector("#descriptionPublished");
-function publishContent() {
-  
-  let song = new Song(inputTitle.value, inputArtist.value, inputDescription.value, localStorage.getItem("input-file"));
-  console.log(song._title);
+function loadPost(song) {
+  const mainDiv = document.querySelector("#bacheca");
+  const newDiv = document.createElement("div");
   const newImage = document.createElement("img");
   const newTitle = document.createElement("p");
   const newArtist = document.createElement("p");
@@ -66,17 +63,46 @@ function publishContent() {
   newImage.setAttribute("width", 128);
   newImage.style.margin = "10px"
   
-  newImage.setAttribute('src', song._image);
-  imagePublished.appendChild(newImage);
-  newTitle.innerHTML = song._title;
-  newArtist.innerHTML = song._artist;
-  newDescription.innerHTML = song._description;
+  newImage.setAttribute('src', song.image);
+  newDiv.appendChild(newImage);
+  newTitle.innerHTML = song.title;
+  newArtist.innerHTML = song.artist;
+  newDescription.innerHTML = song.description;
 
-  firstColumn.appendChild(newTitle);
-  secondColumn.appendChild(newArtist);
-  thirdColumn.appendChild(newDescription);
+  newDiv.appendChild(newTitle);
+  newDiv.appendChild(newArtist);
+  newDiv.appendChild(newDescription);
+  newDiv.style.display = "flex";
+  newDiv.style.flexDirection = "row";
+  newDiv.style.justifyContent = "space-around";
+  newDiv.style.alignItems = "center";
+  mainDiv.appendChild(newDiv);
 
   input.style.display = "block";
   myImage.style.display = "none";
   input.value = "";
+}
+function publishContent() {
+  let counterDB = fetchLocalDB();
+  let song = {title: inputTitle.value, artist: inputArtist.value, 
+    description: inputDescription.value, image: localStorage.getItem("image")};
+  localStorage.setItem("song"+counterDB, JSON.stringify(song));
+  console.log(JSON.parse(localStorage.getItem("song"+counterDB)));
+  counterDB++;
+  localStorage.setItem("counterDB",counterDB);  console.log(song._title);
+  loadPost(song);
+}
+
+function fetchLocalDB(counterDB) {
+  if(!localStorage.getItem("counterDB")) {
+    localStorage.setItem("counterDB",1);
+    //console.log("Il localDB non contiene il contatore");
+  } else return parseInt(localStorage.getItem("counterDB"));
+}
+
+function fetchPost() {
+  let counterDB = parseInt(localStorage.getItem("counterDB"));
+  for(let i = 1; i < counterDB; i++) {
+    loadPost(JSON.parse(localStorage.getItem("song"+i)));
+  }
 }
